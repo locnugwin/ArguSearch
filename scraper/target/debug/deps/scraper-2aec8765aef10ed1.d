da7a5d17f9c00fc775f5137ecfd92907c0ff9b2b
Loc Nguyen <lnguyen72@huskers.unl.edu>
2024-10-20 21:34:27 -0500

    Initialize scraper

---
+++ scraper/target/debug/deps/scraper-2aec8765aef10ed1.d
@@ -0,0 +1,5 @@
+C:\Users\Loc\Desktop\Projects\ArguSearch\scraper\target\debug\deps\libscraper-2aec8765aef10ed1.rmeta: src/main.rs
+
+C:\Users\Loc\Desktop\Projects\ArguSearch\scraper\target\debug\deps\scraper-2aec8765aef10ed1.d: src/main.rs
+
+src/main.rs:
---
+++ scraper/target/debug/deps/scraper-f8ffb76caddd53bd.d
@@ -0,0 +1,5 @@
+C:\Users\Loc\Desktop\Projects\ArguSearch\scraper\target\debug\deps\libscraper-f8ffb76caddd53bd.rmeta: src/main.rs
+
+C:\Users\Loc\Desktop\Projects\ArguSearch\scraper\target\debug\deps\scraper-f8ffb76caddd53bd.d: src/main.rs
+
+src/main.rs:
